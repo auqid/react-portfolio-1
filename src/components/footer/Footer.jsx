d3bdb918fc2219e2b74e7f745ba1918e7d67a431
auqid <38567390+auqid@users.noreply.github.com>
2023-03-12 17:01:00 +0530

    chnages to footer typo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
-import './footer.css'
-import {BsLinkedin,BsGithub,BsInstagram,BsYoutube} from 'react-icons/bs'
-
+import React from "react";
+import "./footer.css";
+import { BsLinkedin, BsGithub, BsInstagram, BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      <a href='#top' className='footer__logo'>Auqid Irfan</a>
-      <ul className='permalinks'>
-        <li><a href='#top'>Home</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#experience'>Experience</a></li>
-        <li><a href='#portfolio'>Portfolio</a></li>
-        <li><a href='#contact'>Portfolio</a></li>
-
-
+      <a href="#top" className="footer__logo">
+        Auqid Irfan
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#top">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
-      <div className='footer__socials'>
-      <a href='https://www.linkedin.com/in/auqidirfan/' target='blank'><BsLinkedin/></a>
-        <a href='https://github.com/auqid' target='blank'><BsGithub/></a>
-        <a href='https://instagram.com/auqid' target='blank'><BsInstagram/></a>
-        <a href='https://www.youtube.com/watch?v=xvFZjo5PgG0' target='blank'><BsYoutube/></a>
-
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/auqidirfan/" target="blank">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/auqid" target="blank">
+          <BsGithub />
+        </a>
+        <a href="https://instagram.com/auqid" target="blank">
+          <BsInstagram />
+        </a>
+        <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="blank">
+          <BsYoutube />
+        </a>
       </div>
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
